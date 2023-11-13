9df3d09ee27ae47754a0aeecc36fa8d3a9a7de94
tlkhanh <longkhanhta@gmail.com>
2023-11-13 22:36:40 +0700

    Services Configuration

--- RestFullAPI/Program.cs
+++ RestFullAPI/Program.cs
@@ -1,12 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.HttpOverrides;
+using RestFullAPI.ServiceExtensions;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
+builder.Services.ConfigureCors();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(
+    new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.All
+    }
+);
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
